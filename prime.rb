17af47d63d94814032bd3c5dfff4793785f2d007
Jc Dior <jessicaclairdior@gmail.com>
2020-09-02 19:26:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,8 @@
 require 'prime'
 
 def prime?(num)
- n = 2
- while n < num
-   return false if num % n == 0
-   n += 1
- end
- true
+  if num.prime?
+    return true
+  else return false
+  end
 end
